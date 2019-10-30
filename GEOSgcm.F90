75b52e651615108a681f0d3166d081ded2c11a83
Kyle Gerheiser <kyle.gerheiser@nasa.gov>
2019-10-30 11:28:53 -0400

    DAS version for ModelE

--- GEOSgcm.F90
+++ GEOSgcm.F90
@@ -15,18 +15,28 @@ program GEOS5_Main
    use MAPL_Mod
 !!$   use GEOS_GcsGridCompMod, only:  ROOT_SetServices => SetServices
    use GEOS_GcmGridCompMod, only:  ROOT_SetServices => SetServices
+
+   ! populate necessary modele mpi stuff
+   use dist_grid_mod, only: setCommunicator, rank, npes_world
+
    implicit none
+
 !EOP
 
 !EOC
 
    character(len=*), parameter :: Iam="GEOS5_Main"
    type (MAPL_Cap) :: cap
-   type (MAPL_FlapCapOptions) :: cap_options
+   type (MAPL_CapOptions) :: cap_options
    integer :: status
 
-   cap_options = MAPL_FlapCapOptions(description = 'GEOS AGCM', &
-                                     authors     = 'GMAO')
+
+   call MPI_init(status)
+   call setCommunicator(MPI_COMM_WORLD)
+   call MPI_Comm_Size(MPI_COMM_WORLD, npes_world, status)
+   call MPI_Comm_Rank(MPI_COMM_WORLD, rank, status)
+
+   cap_options = MAPL_CapOptions()
    cap = MAPL_Cap('GCM', ROOT_SetServices, cap_options = cap_options)
 
    call cap%run(_RC)
