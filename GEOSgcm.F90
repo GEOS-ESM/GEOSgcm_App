45571ac1bb8815d84eecee376ce7a537fe8fff78
Matthew Thompson <matthew.thompson@nasa.gov>
2023-03-06 13:25:19 -0500

    Branch to let setup run

--- GEOSgcm.F90
+++ GEOSgcm.F90
@@ -1,37 +1,10 @@
-! $Id$
-
-! *********************************************************************
-! *****                      Main Program                          ****
-! *****         Finite-Volume Dynamical Core (Lin/Rood)            ****
-! *****         Forced by GEOS5 Physics                            ****
-! *********************************************************************
-
-#define I_AM_MAIN
-
-#include "MAPL_Generic.h"
-
 program GEOS5_Main
-   use MPI
-   use MAPL
-!!$   use GEOS_GcsGridCompMod, only:  ROOT_SetServices => SetServices
-   use GEOS_GcmGridCompMod, only:  ROOT_SetServices => SetServices
    implicit none
-!EOP
-
-!EOC
 
    character(len=*), parameter :: Iam="GEOS5_Main"
-   type (MAPL_Cap) :: cap
-   type (MAPL_FlapCLI) :: cli
-   type (MAPL_CapOptions) :: cap_options
    integer :: status
 
-   cli = MAPL_FlapCLI(description = 'GEOS AGCM', &
-                                     authors     = 'GMAO')
-   cap_options = MAPL_CapOptions(cli)
-   cap = MAPL_Cap('GCM', ROOT_SetServices, cap_options = cap_options)
-
-   call cap%run(_RC)
+   write (*,*) "I am ", Iam
 
 end program GEOS5_Main
    
